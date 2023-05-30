f1fa5c406e93ef60e38e130812014206f4b7ca61
Lucas Fraga <ulucasfraga@gmail.com>
2023-05-30 12:41:18 +0100

    fix locators and lib

--- playwright.config.js
+++ playwright.config.js
@@ -1,18 +1,18 @@
-import { expect } from "@playwright/test";
-require("dotenv").config({ path: ".env" });
+import { expect } from '@playwright/test'
+require('dotenv').config({ path: '.env' })
 
-const toMatchSchema = require("./lib/expect-schema");
-expect.extend(toMatchSchema);
+const toMatchSchema = require('./lib/expect-schema')
+expect.extend(toMatchSchema)
 
-const apiServerest = process.env.API_SERVEREST;
-const e2eServerest = process.env.E2E_SERVEREST;
+const apiServerest = process.env.API_SERVEREST
+const e2eServerest = process.env.E2E_SERVEREST
 
-process.env.PLAYWRIGHT_EXPERIMENTAL_FEATURES = "1";
+process.env.PLAYWRIGHT_EXPERIMENTAL_FEATURES = '1'
 
-export const retries = process.env.CI ? 1 : 0;
+export const retries = process.env.CI ? 1 : 0
 export const reporter = [
   [
-    "allure-playwright",
+    'allure-playwright',
     {
       environmentInfo: {
         E2E_NODE_VERSION: process.version,
@@ -20,38 +20,38 @@ export const reporter = [
       },
     },
   ],
-  ["list"],
-  ["html", { open: "never", outputFolder: "reports" }],
-];
+  ['list'],
+  ['html', { open: 'never', outputFolder: 'reports' }],
+]
 export const projects = [
   {
-    name: "api",
-    testMatch: "**/api/*/*.api.test.js",
+    name: 'api',
+    testMatch: '**/api/*/*.api.test.js',
     use: {
       baseURL: apiServerest,
     },
   },
   {
-    name: "e2e",
-    outputDir: "test-results",
-    testMatch: "**/e2e/*/*.e2e.test.js",
+    name: 'e2e',
+    outputDir: 'test-results',
+    testMatch: '**/e2e/*/*.e2e.test.js',
     use: {
       baseURL: e2eServerest,
-      browsers: ["chromium"],
+      browsers: ['chromium'],
       viewport: { width: 1440, height: 900 },
-      screenshot: "only-on-failure",
-      trace: "retain-on-failure",
+      screenshot: 'only-on-failure',
+      trace: 'retain-on-failure',
       bypassCSP: true,
       launchOptions: {
         args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-web-security",
-          "--disable-gpu",
-          "--disable-dev-shm-usage",
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-web-security',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
         ],
         headless: true,
       },
     },
   },
-];
+]
--- tests/e2e/login/login.e2e.test.js
+++ tests/e2e/login/login.e2e.test.js
@@ -19,27 +19,27 @@ test.describe.parallel('[LOGIN] :: SERVEREST - E2E', () => {
   })
 
   test('login with invalid email', async ({ page }) => {
-    await page.type('data-testid=email', data.email)
-    await page.type('data-testid=senha', password)
-    await page.click('data-testid=entrar')
+    await page.type('id=email', data.email)
+    await page.type('id=password', password)
+    await page.click('button[type="submit"]')
 
     const error = page.locator('role=alert')
     await expect(error).toBeVisible()
   })
 
   test('login with invalid password', async ({ page }) => {
-    await page.type('data-testid=email', user)
-    await page.type('data-testid=senha', data.password)
-    await page.click('data-testid=entrar')
+    await page.type('id=email', user)
+    await page.type('id=senha', data.password)
+    await page.click('button[type="submit"]')
 
     const error = page.locator('role=alert')
     await expect(error).toHaveCount(1)
   })
 
   test('login with blank data', async ({ page }) => {
-    await page.type('data-testid=email', '')
-    await page.type('data-testid=senha', '')
-    await page.click('data-testid=entrar')
+    await page.type('id=email', '')
+    await page.type('id=senha', '')
+    await page.click('button[type="submit"]')
 
     const error = page.locator('role=alert')
     await expect(error).toHaveCount(2)
